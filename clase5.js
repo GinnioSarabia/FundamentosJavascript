b7624dcd96a78a43ea20d19d3be327c9e0490db5
Ginnio Sarabia <sarabia.seven@gmail.com>
2020-04-05 16:18:14 -0500

    funcione y un poco mas

--- clase5.js
+++ clase5.js
@@ -1,8 +1,20 @@
-var nombre = 'Ginnio'
+var persona = {
+    nombre: 'Ginnio',
+    apellido: 'Sarabia',
+    edad: 28
+}
 
 function imprimirNombreEnMayusculas(n) {
     n = n.toUpperCase()
     console.log(n)
 }
 
-imprimirNombreEnMayusculas(nombre)
+imprimirNombreEnMayusculas(persona.nombre)
+
+function cumpleanos(persona) {
+    return {
+        ...persona,
+        edad : persona.edad + 1
+    }
+}
+
