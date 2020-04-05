bbbadeefb4d4e6cbaa3d3d4dcc5e75e62c7993a4
Ginnio Sarabia <sarabia.seven@gmail.com>
2020-04-05 12:45:14 -0500

    Clase terminada de String en JavaScript

--- clase1.js
+++ clase1.js
@@ -1,7 +1,7 @@
 var nombre = 'Ginnio'
 var apellido = 'Sarabia'
-var edad = 28
 var peso = 75
+var edad = 28
 
 var nombreEnMayusculas = nombre.toUpperCase()
 var apellidoEnMinisculas = apellido.toLowerCase()
@@ -13,3 +13,44 @@ var nombreCompleto2 = `${nombre.toUpperCase()} ${apellido.toUpperCase()}`
 var substringNombre = nombre.substr(1, nombre.length-1)
 
 console.log('Hola ' + nombre + ' ' + apellido + "\nMi edad es: " + edad)
+
+edad += 1
+
+console.log(edad)
+
+peso = peso - 2
+
+console.log(peso)
+
+var sandwich = 2
+
+peso = peso + sandwich
+
+console.log(peso)
+
+var jugarAlFultbol = 3
+
+peso = peso - jugarAlFultbol
+
+console.log(peso)
+
+var precioVino = 200.3
+
+var total = precioVino * 3
+
+console.log(total)
+
+total = precioVino * 100 * 3 / 100
+
+
+total = Math.round(precioVino * 100 * 3)/100
+
+
+console.log(total)
+
+var totalStr = total.toFixed(2)
+
+//Pizza
+var pizza = 8
+var persona = 2
+var cantidadDePorcionesPorPersona = pizza / persona
