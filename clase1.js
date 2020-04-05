2f4e246cc349aef640e4531eea9d80c17119d851
Ginnio Sarabia <sarabia.seven@gmail.com>
2020-04-05 12:28:17 -0500

    Finalizada la clase de Strings en Javascript

--- clase1.js
+++ clase1.js
@@ -3,4 +3,13 @@ var apellido = 'Sarabia'
 var edad = 28
 var peso = 75
 
-console.log('Hola ' + nombre + ' ' + apellido + "Mi edad es: " + edad)
+var nombreEnMayusculas = nombre.toUpperCase()
+var apellidoEnMinisculas = apellido.toLowerCase()
+var primeraLetraNombre = nombre.charAt(0)
+var cantidadDeLetrasDelNombre = nombre.length
+var ultimaLetraDelNombre = nombre.charAt(nombre.length - 1)
+var nombreCompleto = nombre + ' ' + apellido  
+var nombreCompleto2 = `${nombre.toUpperCase()} ${apellido.toUpperCase()}`
+var substringNombre = nombre.substr(1, nombre.length-1)
+
+console.log('Hola ' + nombre + ' ' + apellido + "\nMi edad es: " + edad)
