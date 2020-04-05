b7a4745c4f0233a01754c57b31b9f29d2c50b4ad
Ginnio Sarabia <sarabia.seven@gmail.com>
2020-04-05 15:33:32 -0500

    Varias clases del curso de Javascript

---
+++ funciones.js
@@ -0,0 +1,11 @@
+var nombre = 'Ginnio'
+var edad = 28
+
+function imprimirEdad(n, e){
+    //Cuerpo de la función
+    console.log(`${n} tiene ${e} años`)
+}
+
+imprimirEdad(nombre, edad)
+imprimirEdad('Joselyn', 27)
+imprimirEdad('Damian', 2)
